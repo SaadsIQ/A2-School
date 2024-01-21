6881efe3fb6d5f45a4f6c19a14955460cb484fab
Saad Asif <saadbinasif.mail@gmail.com>
2024-01-21 14:51:19 +0200

    Initial Commit

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+array = [42, 17, 89, 5, 73, 28, 64, 11, 36, 50]
+n = len(array)
+flag = True
+
+print(array)
+
+while flag:
+    flag = False
+    for i in range(0, n-1):
+        if array[i] > array[i+1]:
+            array[i],array[i+1] = array[i+1], array[i]
+            flag = True
+            
+print(array)
---
+++ bubbleSort2d.py
@@ -0,0 +1,14 @@
+name = [["smithfield","tom"],["john","smith"],["bin","laden"],["saddam","hussein"],["suicide","bomber"]]
+score = [24,55,68,84,100]
+student = len(name)
+flag = True
+
+while flag:
+    flag = False
+    for x in range(0, student-1):
+        if score[x] < score [x+1]:
+            score[x],score[x+1] = score[x+1], score[x]
+            name[x],name[x+1] = name[x+1],name[x]
+            flag = True
+print(name)
+print(score)
