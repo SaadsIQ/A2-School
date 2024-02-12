baed53c3fca6e96d1fa584dae3337ee21fab4b3d
Saad Asif <saadbinasif.mail@gmail.com>
2024-02-12 13:15:44 +0200

    insertion sort

---
+++ SortingAlgorithms/insertionSort.py
@@ -0,0 +1,12 @@
+items = [1,53,62,67,86,42,7]
+n = len(items)
+
+for index in range(1,n):
+    currentItem = items[index]
+    index2 = index - 1
+    while index2 >= 0 and currentItem < items[index2]:
+        items[index2 +1] = items[index2]
+        index2 -=1
+        items[index2 +1] = currentItem
+
+print(items)
