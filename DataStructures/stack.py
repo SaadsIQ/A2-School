91d0b848ef2ae0a53e557ced71e7d6e958ba61a6
Saad Asif <saadbinasif.mail@gmail.com>
2024-02-14 17:59:38 +0200

    commit message

--- DataStructures/stack.py
+++ DataStructures/stack.py
@@ -4,16 +4,29 @@ def push():
         topPointer = topPointer + 1
         item = int(input("input a number: "))
         arr[topPointer] = item
+        print("Item pushed: ", item)
+    else: 
+        print("Stack full cannot push")
+
+def pop():
+    global topPointer, basePointer,item, stackFull
+    if topPointer >= basePointer:
+        item = arr[topPointer]
+        topPointer -= 1
+        print("Popped item: ", item)
+        print(arr)
 
 arr = [None for index in range(0,10)]
 basePointer = 0
 topPointer = -1
 stackFull = len(arr)
 item = None
-print(stackFull)
+
 numPush = int(input("Enter the number of items to push: "))
 for _ in range(numPush):
     push()
 numPop = int(input("Enter the number of items to pop: "))
 for _ in range(numPop):
     pop()
+
+print(arr)
