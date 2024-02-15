cf86ad3c1d2749ffcc40134d3e754c49a6e8f94f
Saad Asif <saadbinasif.mail@gmail.com>
2024-02-15 14:04:47 +0200

    start dequeue

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -9,6 +9,21 @@ def enQueue():
         arr[rearPointer] = item
         print("Item enqueued: ", item)
 
+def deQueue():
+    global arr, frontPointer, rearPointer, queueLength, queueSize, item
+    if queueLength == 0:
+        print("Queue full cannot dequeue")
+    else:
+        item = arr[frontPointer]
+        arr[frontPointer] = None
+        frontPointer += 1
+        queueLength -= 1
+        print("Item dequeued: ", item)
+        for index in range(0, queueLength):
+            arr[index], arr[index+1] = arr[index+1], arr[index]
+            index = index + 1
+
+
 arr = [None for index in range(0,int(input("Enter Queue size: ")))]
 frontPointer = 0
 rearPointer = -1
@@ -19,7 +34,7 @@ item = None
 numEnqueue = int(input("Enter the number of items to enqueue: "))
 for _ in range(numEnqueue):
     enQueue()
-#numDequeue = int(input("Enter the number of items to dequeue: "))
-#for _ in range(numdeQueue):
-#    deQueue()
+numDequeue = int(input("Enter the number of items to dequeue: "))
+for _ in range(numDequeue):
+    deQueue()
 print(arr)
