b2464170a4c6345f197bfcb2843c9a92b37104d3
Saad Asif <saadbinasif.mail@gmail.com>
2024-02-20 11:28:43 +0200

    circular queue

--- DataStructures/circularQueue.py
+++ DataStructures/circularQueue.py
@@ -3,7 +3,6 @@ def enQueue():
     if queueLength == queueSize:
         print("Queue full cannot enqueue:")
     else:
-
         if rearPointer == queueSize:
             rearPointer = 0
         if frontPointer == -1:
@@ -12,6 +11,17 @@ def enQueue():
         queueLength = queueLength + 1
         item = int(input("input a number: "))
         arr[rearPointer] = item
+def deQueue():
+    global arr, frontPointer, rearPointer, queueSize, queueLength
+    if queueLength == 0:
+        print("Queue empty cannot dequeue")
+    else:
+        item = arr[frontPointer]
+        arr[frontPointer] = None
+        frontPointer +=1
+        queueLength -=1
+        arr[:] = arr[frontPointer:]
+
 
 arr = [None for _ in range(0,int(input("Enter Queue size: ")))]
 frontPointer = -1
@@ -22,4 +32,9 @@ queueLength = 0
 numEnqueue = int(input("Enter the number of items to enqueue: "))
 for _ in range(numEnqueue):
     enQueue()
+print(arr)
+
+numDequeue = int(input("Enter the number of items to dequeue: "))
+for _ in range(numDequeue):
+    deQueue()
 print(arr)
