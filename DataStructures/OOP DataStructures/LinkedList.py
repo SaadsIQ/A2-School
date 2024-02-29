d699b51317e2df821c0681a2c639e7442f0271a6
Saad Asif <saadbinasif.mail@gmail.com>
2024-02-29 09:28:57 +0200

    finish insert at index

--- DataStructures/OOP DataStructures/LinkedList.py
+++ DataStructures/OOP DataStructures/LinkedList.py
@@ -6,6 +6,8 @@ class Node:
 class linkedList:
     def __init__(self):
         self.head = None
+        self.pointer = None
+        self.pointerArray = []
     def insertAtStart(self, value):
         newNode = Node(value)
         if self.head == None:
@@ -24,7 +26,23 @@ class linkedList:
             currentNode = currentNode.next
         currentNode.next = newNode
         return
-    def insertAtIndex(self, value):
+    def insertAtIndex(self, value, index):
+        newNode = Node(value)
+        currentNode = self.head
+        position = 0
+        if position == index:
+            self.insertAtStart(value)
+            return
+        else:
+            while currentNode != None and position + 1 != index:
+                position = position + 1
+                currentNode = currentNode.next
+            if currentNode != None:
+                newNode.next = currentNode.next
+                currentNode.next = newNode
+            else:
+                print("Index out of range")
+
     def printLL(self):
         currentNode = self.head
         while(currentNode):
@@ -35,7 +53,7 @@ llist = linkedList()
 llist.insertAtStart(1)
 llist.insertAtStart(2)
 llist.insertAtStart(1)
-llist.insertAtEnd(2)
-
+llist.insertAtEnd(3)
+llist.insertAtIndex(4,2)
 print("Node Data")
 llist.printLL()
