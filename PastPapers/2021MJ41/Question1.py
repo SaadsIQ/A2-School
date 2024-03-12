724c2f7246479c53bbe7fe16d3fb26501d28c39d
Saad Asif <saadbinasif.mail@gmail.com>
2024-03-12 11:32:37 +0200

    q1 completed

--- PastPapers/2021MJ41/Question1.py
+++ PastPapers/2021MJ41/Question1.py
@@ -8,9 +8,29 @@ class LinkedList:
         self.linkedList = [node(1,1), node(5,4), node(6,7), node(7,-1), node(2,2), node(0,6), node(0,8), node(56,3), node(0,9), node(0,-1)]
         self.startPointer = 0
         self.emptyList = 5
-    def output_nodes(self, currentPointer):
+            
+    def output_nodes(self):
+        currentPointer = self.startPointer
         while currentPointer != -1:
             print(str(self.linkedList[currentPointer].data))
             currentPointer = self.linkedList[currentPointer].next
+                
+    def addNode(self):
+        dataToAdd = input("Enter the data to add: ")
+        if self.emptyList <0 and self.emptyList > 9:
+            return False
+        else:
+            newNode = node(int(dataToAdd), -1)
+            self.linkedList[self.emptyList] = newNode
+            previousPointer = 0
+            currentPointer = self.startPointer
+            while currentPointer!= -1:
+                previousPointer = currentPointer
+                currentPointer = self.linkedList[currentPointer].next
+            self.linkedList[previousPointer].next = self.emptyList
+            self.emptyList = self.linkedList[self.emptyList].next
+        return True
 myLinkedList = LinkedList()
-myLinkedList.output_nodes(0)
+myLinkedList.output_nodes()
+myLinkedList.addNode()
+myLinkedList.output_nodes()
---
+++ PastPapers/2021MJ41/Question2.py
@@ -0,0 +1,8 @@
+def linearSearch(data):
+    global arrayData
+    index = 0
+    while index < len(arrayData):
+                
+arrayData = [10, 5, 6, 7, 1, 12, 13, 15, 21, 8]
+data = int(input("Enter a number to search for"))
+linearSearch(data)
