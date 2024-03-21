34beed0cf06d8a20bca0460e537086f5f6709638
Saad Asif <saadbinasif.mail@gmail.com>
2024-03-21 09:41:12 +0200

    q3

--- PastPapers/2022MJ42/06_9618_42_Confidential Source Files June 2022/Q3.py
+++ PastPapers/2022MJ42/06_9618_42_Confidential Source Files June 2022/Q3.py
@@ -10,3 +10,17 @@ class card:
         return self.__Number
     def GetColour(self):
         return self.__Colour
+def addData():
+    global Card
+    filename = "CardValues.txt"
+    try:
+        with open(filename,"r") as File:
+            for line in File:
+                Number = line.strip()
+                Colour = File.readline().strip()
+                Card.append(card(Number, Colour))
+    except FileNotFoundError:
+        print("file not found")
+    return Card
+addData()
+print(Card)
