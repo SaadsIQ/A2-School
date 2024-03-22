e36f78724f0570e6f2ea9ac418cf83fae535dc17
Saad Asif <saadbinasif.mail@gmail.com>
2024-03-22 09:40:45 +0200

    q3

--- PastPapers/2022MJ42/06_9618_42_Confidential Source Files June 2022/Q3.py
+++ PastPapers/2022MJ42/06_9618_42_Confidential Source Files June 2022/Q3.py
@@ -1,5 +1,5 @@
-Card = [0 for i in range (30)]
-print(Card)
+cardArray = [0 for i in range (30)]
+print(cardArray)
 class card:
     #Number as Integer
     #Colour as String
@@ -10,17 +10,24 @@ class card:
         return self.__Number
     def GetColour(self):
         return self.__Colour
-def addData():
-    global Card
-    filename = "CardValues.txt"
-    try:
-        with open(filename,"r") as File:
-            for line in File:
-                Number = line.strip()
-                Colour = File.readline().strip()
-                Card.append(card(Number, Colour))
-    except FileNotFoundError:
-        print("file not found")
-    return Card
-addData()
-print(Card)
+def chooseCard():
+    global cardArray
+    flagContinue = True
+    while flagContinue:
+        userCard = int(input("Enter a card number: "))
+        if userCard < 1 or userCard > 30:
+            print("Number must be between 1 and 30")
+        elif NumberChosen[userCard-1]:
+            
+filename = "CardValues.txt"
+try:
+    with open(filename,"r") as File:
+        for x in range(30):
+            Number = int(File.readline())
+            Colour = File.readline().strip()
+            cardArray[x] = card(Number, Colour)
+except FileNotFoundError:
+    print("file not found")
+print(cardArray)
+NumberChosen = [False for i in range(30)]
+print(NumberChosen)
