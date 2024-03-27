444f4e8e05fabaf72b0677332eb9b2e227b803ff
Saad Asif <saadbinasif.mail@gmail.com>
2024-03-27 12:50:09 +0200

    finish q2

--- PastPapers/2021ON42/11_9618_42_Confidential Source Files November 2021/11_9618_42_Confidential Source Files November 2021/Q2.py
+++ PastPapers/2021ON42/11_9618_42_Confidential Source Files November 2021/11_9618_42_Confidential Source Files November 2021/Q2.py
@@ -7,10 +7,10 @@ class Picture:
     
     def GetDescription(self):
         return self.__Description
-    def GetHeight(self):
-        return self.__Height
     def GetWidth(self):
         return self.__Width
+    def GetHeight(self):
+        return self.__Height
     def GetColour(self):
         return self.__FrameColour
 
@@ -22,16 +22,36 @@ def readData(PictureArray):
     counter = 0
     try:
         with open(fileName,"r") as File:
-            Description = File.readline().strip().lower()
-            Width = int(File.readline()).strip()
-            Height = int(File.readline()).strip()
-            FrameColour = File.readline().strip().lower()
-            PictureArray[counter] = picture(Description,Width,Height,FrameColour)
-            counter += 1
+            for line in File:
+                Description = line.strip().lower()
+                Width = int(File.readline().strip())
+                Height = int(File.readline().strip())
+                FrameColour = File.readline().strip().lower()
+                PictureArray[counter] = Picture(Description,Width,Height,FrameColour)
+                counter = counter+1
     except FileNotFoundError:
             print("File not found")
     return counter, PictureArray
 
+def printArray():
+    index1 = 0
+    for _ in range(NumberPicturesInArray):
+        print(PictureArray[index1].GetColour(),PictureArray[index1].GetWidth(),PictureArray[index1].GetHeight(),PictureArray[index1].GetDescription())
+        index1 += 1
+
 PictureArray = [Picture("",0,0,"") for x in range(0,100)]
-print(PictureArray)
-readData()
+
+NumberPicturesInArray, PictureArray = readData(PictureArray)
+
+print(NumberPicturesInArray)
+printArray()
+
+usercolour = str(input("Enter a color to search for: ")).lower()
+userWidth = int(input("Enter the maximum width: "))
+userHeight = int(input("Enter the maximum height: "))
+
+for x in range(NumberPicturesInArray):
+    if PictureArray[x].GetColour() == usercolour:
+        if PictureArray[x].GetWidth() <= userWidth:
+            if PictureArray[x].GetHeight() <= userHeight:
+                print(PictureArray[x].GetDescription(), PictureArray[x].GetWidth(), PictureArray[x].GetHeight(), PictureArray[x].GetColour())
