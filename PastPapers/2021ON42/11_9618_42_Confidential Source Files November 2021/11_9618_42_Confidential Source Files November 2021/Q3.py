e981c71b2fe5c2cf4438e387557573c1a77a7bf4
Saad Asif <saadbinasif.mail@gmail.com>
2024-03-28 09:44:03 +0200

    q3

---
+++ PastPapers/2021ON42/11_9618_42_Confidential Source Files November 2021/11_9618_42_Confidential Source Files November 2021/Q3.py
@@ -0,0 +1,40 @@
+def AddNode(ArrayNode, RootPointer , FreeNode):
+    NodeData = int(input("Enter the Data: "))
+    if FreeNode <= 19:
+        ArrayNode[FreeNode][0] = -1
+        ArrayNode[FreeNode][1] = NodeData
+        ArrayNode[FreeNode][2] = -1
+        if RootPointer == -1:
+            RootPointer = 0
+        else:
+            Placed = False
+            CurrentNode = RootPointer
+            while not Placed:
+                if NodeData < ArrayNode[CurrentNode][1]:
+                    if ArrayNode[CurrentNode][0] ==-1 :
+                        ArrayNode[CurrentNode][0] = FreeNode
+                        Placed = True
+                    else:
+                        CurrentNode = ArrayNode[CurrentNode][0]
+                else:
+                    if ArrayNode[CurrentNode][2] == -1:
+                        ArrayNode[CurrentNode][2] = FreeNode
+                        Placed = True
+                    else:
+                        CurrentNode = ArrayNode[CurrentNode][2]
+        FreeNode += 1
+    else:
+        print("Tree is full")
+    return ArrayNode, RootPointer, FreeNode
+
+
+    
+def printAll(ArrayNode):
+    for x in range(0,20):
+        print(ArrayNode[x][0]," ", ArrayNode[x][1], " ", ArrayNode[x][2])
+ArrayNode = [[0 for x in range (3)] for y in range (20)] #integer
+RootPointer = -1 #integer
+FreeNode = 0 #integer
+for x in range (10):
+    ArrayNode,RootPointer,FreeNode = AddNode(ArrayNode, RootPointer, FreeNode)
+printAll(ArrayNode)
