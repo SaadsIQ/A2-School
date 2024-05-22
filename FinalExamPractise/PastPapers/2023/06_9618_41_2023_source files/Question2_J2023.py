3e084f1a7108e29faeb8ab8ed2c7c90df59a43d2
Saad Asif <saadbinasif.mail@gmail.com>
2024-05-22 22:36:25 +0300

    a

--- FinalExamPractise/PastPapers/2023/06_9618_41_2023_source files/Question2_J2023.py
+++ FinalExamPractise/PastPapers/2023/06_9618_41_2023_source files/Question2_J2023.py
@@ -54,3 +54,67 @@ Car.OutputValues()
 Heli.IncreaseSpeed()
 Heli.IncreaseSpeed()
 Heli.OutputValues()
+
+#vehicle mr oz
+"""
+
+class Vehicle:
+    def __init__(self, ID, MaxSpeed, IncreaseAmount):
+        self._ID = ID  # Changed to protected
+        self._MaxSpeed = MaxSpeed  # Changed to protected
+        self._IncreaseAmount = IncreaseAmount  # Changed to protected
+        self._CurrentSpeed = 0  # Changed to protected
+        self._HorizontalPosition = 0  # Changed to protected
+
+    def GetCurrentSpeed(self):
+        return self._CurrentSpeed
+
+    def GetIncreaseAmount(self):
+        return self._IncreaseAmount
+
+    def GetMaxSpeed(self):
+        return self._MaxSpeed
+
+    def GetHorizontalPosition(self):
+        return self._HorizontalPosition
+
+    def SetCurrentSpeed(self, CurrentSpeed):
+        self._CurrentSpeed = CurrentSpeed
+        if self._CurrentSpeed > self._MaxSpeed:
+            self._CurrentSpeed = self._MaxSpeed
+        self._HorizontalPosition += self._CurrentSpeed
+
+    def IncreaseSpeed(self):
+        self.SetCurrentSpeed(self._CurrentSpeed + self._IncreaseAmount)
+
+    def OutputValues(self):
+        print("Current position = ", self.GetHorizontalPosition())
+        print("Current speed = ", self.GetCurrentSpeed())
+
+class Helicopter(Vehicle):
+    def __init__(self, ID, MaxSpeed, IncreaseAmount, VerticalChange, MaximumHeight):
+        super().__init__(ID, MaxSpeed, IncreaseAmount)
+        self._VerticalPosition = 0  # Changed to protected
+        self._VerticalChange = VerticalChange  # Changed to protected
+        self._MaximumHeight = MaximumHeight  # Changed to protected
+
+    def IncreaseSpeed(self):
+        self._VerticalPosition += self._VerticalChange
+        if self._VerticalPosition > self._MaximumHeight:
+            self._VerticalPosition = self._MaximumHeight
+        super().IncreaseSpeed()
+
+    def OutputValues(self):
+        super().OutputValues()
+        print("Vertical position = ", self._VerticalPosition)
+
+# Example usage
+Car = Vehicle("Tiger", 100, 20)
+Heli = Helicopter("Lion", 350, 40, 3, 100)
+Car.IncreaseSpeed()
+Car.IncreaseSpeed()
+Car.OutputValues()
+Heli.IncreaseSpeed()
+Heli.IncreaseSpeed()
+Heli.OutputValues()
+"""
